cddc0b612578776b8115b8e806d0964b7e6b0efc
matolala <martinsadagbusi@yahoo.com>
2025-03-22 06:41:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "demo_subnet" {
   vpc_id     = aws_vpc.demo.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "demo_subnet"
